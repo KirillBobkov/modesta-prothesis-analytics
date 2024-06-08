74fa028c59f19bf4d449ade2261565d165557ffd
Kirill Bobkov <kirill_2105@mail.ru>
2024-06-08 14:28:27 +0400

    comments

--- src/connect.tsx
+++ src/connect.tsx
@@ -10,35 +10,35 @@ let serialCharacteristic: any;
 export function connect() { 
     console.log('Запрос погнал'); 
  
-
-    return interval(1000);
-    // const device$ = from(navigator.bluetooth.requestDevice({ 
-    //     filters: [{ 
-    //         services: [serviceUUID] 
-    //     }] 
-    // })); 
+    // @ts-ignore
+    const device$ = from(navigator.bluetooth.requestDevice({ 
+        filters: [{ 
+            services: [serviceUUID] 
+        }] 
+    })); 
  
-    // const server$ = device$.pipe( 
-    //     switchMap(device => from(device.gatt.connect())), 
-    //     tap(() => console.log('Берем сервис ', serviceUUID)), 
-    //     switchMap(server => from(server.getPrimaryService(serviceUUID))), 
-    //     tap(() => console.log('Берем характеристику ', serialUUID)), 
-    //     switchMap(service => from(service.getCharacteristic(serialUUID))), 
-    //     tap(characteristic => { 
-    //         serialCharacteristic = characteristic; 
-    //         console.log('Подписываемся на сообщения '); 
-    //     }) 
-    // ); 
+    const server$ = device$.pipe( 
+        switchMap(device => from(device.gatt.connect())), 
+        tap(() => console.log('Берем сервис ', serviceUUID)), 
+          // @ts-ignore
+        switchMap(server => from(server.getPrimaryService(serviceUUID))), 
+        tap(() => console.log('Берем характеристику ', serialUUID)), 
+        switchMap(service => from(service.getCharacteristic(serialUUID))), 
+        tap(characteristic => { 
+            serialCharacteristic = characteristic; 
+            console.log('Подписываемся на сообщения '); 
+        }) 
+    ); 
  
-    // const notifications$ = server$.pipe( 
-    //     switchMap(() => from(serialCharacteristic.startNotifications())), 
-    //     switchMap(() => fromEvent(serialCharacteristic, 'characteristicvaluechanged')), 
-    //     tap(() => { 
-    //         serialCharacteristic.addEventListener('characteristicvaluechanged', read); 
-    //     }) 
-    // ); 
+    const notifications$ = server$.pipe( 
+        switchMap(() => from(serialCharacteristic.startNotifications())), 
+        switchMap(() => fromEvent(serialCharacteristic, 'characteristicvaluechanged')), 
+        tap(() => { 
+            serialCharacteristic.addEventListener('characteristicvaluechanged', read); 
+        }) 
+    ); 
  
-    // return notifications$; 
+    return notifications$; 
 }
 
 function disconnect(){
